0c6445ed1e167d516b8733c3c626d80fb32b149e
Alexander Williams <alexanderwilliams@Alexanders-Laptop.local>
2024-09-15 11:41:05 -0400

    players: adds websocket to populate player data
    sends full player data on initial connection and when registrations
    occur in other locations

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
     "github.com/gin-gonic/gin"
     "github.com/gin-contrib/cors"
+    "github.com/gorilla/websocket"
+    "container/list"
     "fmt"
+    "net/http"
 )
 
 type Role int
@@ -17,27 +20,107 @@ const (
 type Player struct {
     Name string
     Number string
-    Type Role
+    role Role
+    Alive bool
 }
 
-var players = make(map[string]*Player)
+type PlayerList struct {
+    Players []Player
+}
+
+var players = make(map[string]Player)
+
+var connChannels = list.New()
+
+var upgrader = websocket.Upgrader{
+    ReadBufferSize: 1024,
+    WriteBufferSize: 1024,
+    CheckOrigin: func(r *http.Request) bool {
+        origin := r.Header.Get("Origin")
+        return origin == "http://localhost:5173"
+    },
+}
 
 func register(c *gin.Context) {
-    p := new(Player)
-    p.Name = c.PostForm("name") 
-    p.Number = c.PostForm("number")
-    p.Type = ROLE_UNASSIGNED
+    // Add player to playermap
+    p := Player{
+        Name: c.PostForm("name"),
+        Number: c.PostForm("number"),
+        role: ROLE_UNASSIGNED,
+        Alive: true,
+    }
 
     players[p.Name] = p
     for k, _ := range players {
         fmt.Println(k, players[k])
     }
+    for e := connChannels.Front(); e != nil; e = e.Next() {
+        if channel, ok := e.Value.(chan bool); ok {
+            channel <- true
+        }
+    }
     c.Status(200)
+}
+
+func flatPlayers() PlayerList {
+    flatList := make([]Player, 0, len(players))
+    for  _, v := range players {
+        flatList = append(flatList, v)
+    }
+
+    p := PlayerList{
+        Players: flatList,
+    }
+    fmt.Println(p)
+
+    return p 
+}
+
+func socketReader(ws *websocket.Conn, comms chan bool) {
+    _, _, err := ws.ReadMessage()
+    if err != nil {
+        fmt.Println("Closing Reader")
+        comms <- false
+        return
+    }
+}
+
+func playerWS(c *gin.Context) {
+    // upgrade connection to websocket
+    ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+    if err != nil {
+        fmt.Println("Failed to upgrade", err)
+        return
+    }
+    updateChan := make(chan bool)
+    e := connChannels.PushBack(updateChan)
+    defer ws.Close()
+    defer connChannels.Remove(e)
+    
+    err = ws.WriteJSON(flatPlayers())
+    if err != nil {
+        fmt.Println("Failed to write playerlist on initial conn", err)
+        return
+    }
+    go socketReader(ws, updateChan)
+    for {
+        msg := <- updateChan
+        if !msg {
+            fmt.Println("Closing Writer")
+            return
+        }
+        err = ws.WriteJSON(flatPlayers())
+        if err != nil {
+            fmt.Println("Failed to write playerlist from chan update", err)
+            return
+        }
+    }
 }
 
 func main() {
     router := gin.Default()
     router.Use(cors.Default())
     router.POST("/register", register)
+    router.GET("/players", playerWS)
     router.Run(":8080")
 }
