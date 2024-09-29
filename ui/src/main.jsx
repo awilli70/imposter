3c547f2dc2e4620451ad7ae47e3330eb00147db7
Alexander Williams <alexanderwilliams@Alexanders-Laptop.local>
2024-09-29 16:54:46 -0400

    admin: add start, stop, and reset actions

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom"
 import Players from './views/players.jsx'
 import Register from './views/register.jsx'
+import Admin from './views/admin.jsx'
 
 const router = createBrowserRouter([
     {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
     {
         path: "/register",
         element: <Register />
+    },
+    {
+        path: "/admin",
+        element: <Admin />
     }
 ])
 
--- ui/src/views/players.jsx
+++ ui/src/views/players.jsx
@@ -23,18 +23,30 @@ export default function Players() {
                 <td>
                     <div className="font-bold w-28">{player.Name}</div>
                 </td>
-                <td className="w-full">
-                    {player.Alive ?
-                        <div class="badge bg-lime-500">Alive</div> :
-                        <div class="badge badge-neutral">Dead</div>
-                    }
-                </td>
-                <td>
-                    {player.Alive ?
-                        <button className="btn bg-red-500 hover:bg-red-600 w-24" onClick={() => togglePlayer(player.Name)}>KILL</button> :
-                        <button className="btn btn-neutral w-24" onClick={() => togglePlayer(player.Name)}>RESURRECT</button>
-                    }
-                </td>
+                {Number(player.Role) !== 1 ?
+                    <>
+                        <td className="w-full">
+                            {player.Alive ?
+                                <div class="badge bg-lime-500">Alive</div> :
+                                <div class="badge badge-neutral">Dead</div>
+                            }
+                        </td>
+                        <td>
+                            {player.Alive ?
+                                <button className="btn bg-red-500 hover:bg-red-600 w-24" onClick={() => togglePlayer(player.Name)}>KILL</button> :
+                                <button className="btn btn-neutral w-24" onClick={() => togglePlayer(player.Name)}>RESURRECT</button>
+                            }
+                        </td>
+                    </> :
+                    <>
+                        <td className="w-full">
+                            <div class="badge badge-neutral">Inactive</div>
+                        </td>
+                        <td>
+                            <button className="btn btn-neutral w-24 btn-disabled" onClick={() => togglePlayer(player.Name)}>INACTIVE</button>
+                        </td>
+                    </>
+                }
             </tr>
         )
     }
@@ -48,12 +60,20 @@ export default function Players() {
         return () => wsCurrent.close()
     }, [])
 
+    const playerSort = (a, b) => {
+        if (a.Name == b.Name) {
+            return 0
+        } else if (a.Name.toUpperCase() > b.Name.toUpperCase()) {
+            return 1
+        } else { return -1 }
+    }
+
     return (
         <div className="flex flex-col min-h-screen justify-start items-center pt-4">
             <div className="w-10/12">
                 <div className="content-center w-full join">
-                    <Link className="btn btn-lg btn-primary w-1/2 text-5xl join-item" to={"/players"}><button >Players</button></Link>
-                    <Link className="btn btn-lg btn-tertiary w-1/2 text-5xl join-item" to={"/register"}><button >Register</button></Link>
+                    <Link className="btn btn-lg btn-primary w-1/2 text-4xl join-item" to={"/players"}><button >Players</button></Link>
+                    <Link className="btn btn-lg btn-tertiary w-1/2 text-4xl join-item" to={"/register"}><button >Register</button></Link>
                 </div>
 
                 <div class="card bg-base-100 w-full shadow-xl">
@@ -70,7 +90,22 @@ export default function Players() {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {(players["Players"]) ? players["Players"].map((p) => buildPlayer(p)) : <></>}
+                                    {players["Players"] ?
+                                        <>
+                                            {players["Players"]
+                                                .filter((x) => x.Alive == true && x.Role != 1)
+                                                .sort(playerSort)
+                                                .map((p) => buildPlayer(p))}
+                                            {players["Players"]
+                                                .filter((x) => x.Alive == false && x.Role != 1)
+                                                .sort(playerSort)
+                                                .map((p) => buildPlayer(p))}
+                                            {players["Players"]
+                                                .filter((x) => x.Role == 1)
+                                                .sort(playerSort)
+                                                .map((p) => buildPlayer(p))}
+                                        </> :
+                                        <></>}
                                 </tbody>
                             </table>
                         </div>
